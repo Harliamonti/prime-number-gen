bafa7a49918552d38f1140ec3ac801a342a10c8a
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-28 15:36:54 +0200

    Add improvements

--- main.cpp
+++ main.cpp
@@ -3,35 +3,35 @@
 #include <type_traits>
 #include <concepts>
 
-bool isDivisible(const int num1, const int num2);
-bool isPrime(std::vector<int> &vec, const int num);
-template <typename T>
-bool isValidInput(T input);
+bool isDivisible(const int &num1, const int &num2);
+bool isPrime(std::vector<int> &vec, const int &num);
+bool isTooHigh(const int &num1, const int &num2);
 std::vector<int> generatePrime(int maxNum);
 
 int main() {
-    
+    generatePrime(50);
 }
 
-bool isDivisible(const int num1, const int num2) {
+bool isDivisible(const int &num1, const int &num2) {
     return num1 % num2 ? false : true;
 }
 
-bool isPrime(std::vector<int> &vec, const int num) {
+bool isTooHigh(const int &num1, const int &num2) {
+    if (num2 <= (num1 / num2)) return false;
+    else return true;
+}
+
+bool isPrime(std::vector<int> &vec, const int &num) {
     for (auto &prime : vec) {
-        if (isDivisible(num, prime)) { return false;}
+        if (isTooHigh(num, prime)) return true;
+        else if (isDivisible(num, prime)) return false;
     }
     return true;
 }
 
-template <typename T>
-bool isValidInput(T input) {
-    return std::integral<T>;
-}
-
 std::vector<int> generatePrime(int max) {
-    std::vector<int> primes{ 2, 3 };
-    int current{ 5 };
+    std::vector<int> primes{ 3, 5, 7, 11 };
+    int current{ 13 };
 
     while (current <= max) {
         if (isPrime(primes, current)) {
@@ -40,6 +40,14 @@ std::vector<int> generatePrime(int max) {
         }
         else current += 2;
     }
+
+    for (size_t i{1}; i <= 10; i++) {
+        std::cout << *(primes.end() - i) << std::endl;
+    }
+
+    // for (auto it : primes) {
+    //     std::cout << it << std::endl;
+    // }
 
     return primes;
 }
