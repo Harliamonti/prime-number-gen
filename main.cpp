a481592609baa47aa3676cd93f322514aecb17ee
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-28 11:52:58 +0200

    Moved code from main() to its own function

--- main.cpp
+++ main.cpp
@@ -1,29 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <type_traits>
+#include <concepts>
 
 bool isDivisible(const int num1, const int num2);
 bool isPrime(std::vector<int> &vec, const int num);
+template <typename T>
+bool isValidInput(T input);
+std::vector<int> generatePrime(int maxNum);
 
 int main() {
-
-    std::vector<int> primes{ 2, 3 };
-    int max{ 50 }; //User defined
-    int current{ 5 };
-
-    while (current <= max) {
-
-        if (isPrime(primes, current)) {
-            primes.push_back(current);
-            current += 2;
-        }
-        else current += 2;
-
-    }
-        
-    for (const auto& it : primes)
-    {
-        std::cout << it << std::endl;
-    }
+    
 }
 
 bool isDivisible(const int num1, const int num2) {
@@ -32,9 +19,29 @@ bool isDivisible(const int num1, const int num2) {
 
 bool isPrime(std::vector<int> &vec, const int num) {
     for (auto &prime : vec) {
-        if (isDivisible(num, prime)) {
-            return false;
-        }
+        if (isDivisible(num, prime)) { return false;}
     }
     return true;
 }
+
+template <typename T>
+bool isValidInput(T input) {
+    return std::integral<T>;
+}
+
+std::vector<int> generatePrime(int max) {
+    std::vector<int> primes{ 2, 3 };
+    int current{ 5 };
+
+    while (current <= max) {
+        if (isPrime(primes, current)) {
+            primes.push_back(current);
+            current += 2;
+        }
+        else current += 2;
+    }
+
+    return primes;
+}
+
+//change int to unsigned as we dont care about negative numbers
