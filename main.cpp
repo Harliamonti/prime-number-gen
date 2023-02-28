38fe9be0522008901546b6a54555c1e9bdb458b7
Harliamonti <123170910+Harliamonti@users.noreply.github.com>
2023-02-28 10:27:01 +0200

    Add basic functionality for prime number generator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!";
+bool isDivisible(const int num1, const int num2);
+bool isPrime(std::vector<int> &vec, const int num);
+
+int main() {
+
+    std::vector<int> primes{ 2, 3 };
+    int max{ 50 }; //User defined
+    int current{ 5 };
+
+    while (current <= max) {
+
+        if (isPrime(primes, current)) {
+            primes.push_back(current);
+            current += 2;
+        }
+        else current += 2;
+
+    }
+        
+    for (const auto& it : primes)
+    {
+        std::cout << it << std::endl;
+    }
+}
+
+bool isDivisible(const int num1, const int num2) {
+    return num1 % num2 ? false : true;
+}
+
+bool isPrime(std::vector<int> &vec, const int num) {
+    for (auto &prime : vec) {
+        if (isDivisible(num, prime)) {
+            return false;
+        }
+    }
+    return true;
 }
